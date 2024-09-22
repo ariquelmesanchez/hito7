682c876ed4276f65e24a5af8e3f65338b7a4dc02
korzair <ariquelme.snchz@gmail.com>
2024-09-21 23:16:07 -0300

    se agrega boton de ver detalles a componente cardpizza.jsx

--- src/components/sub-components/CardPizza.jsx
+++ src/components/sub-components/CardPizza.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import { Card, CardBody, CardText, CardTitle } from "react-bootstrap";
+import { Card, CardBody, CardText, CardTitle, Button } from "react-bootstrap"; // Import Button
 import { StoreContext } from "../context/StoreProvider";
 import '../css/Card.css';
 import ButtonsVite from "./ButtonsVite";
 import { CartContext } from "../context/CartContext";
-import { Link } from 'react-router-dom'; // Import Link
+import { Link } from 'react-router-dom';
 
 const CardPizza = () => {
   const { pizzas } = useContext(StoreContext);
@@ -13,33 +13,31 @@ const CardPizza = () => {
   return (
     <div className="cart-container">
       {pizzas.map((pizza) => (
-        <Link 
-          key={pizza.id} 
-          to={`/pizza/${pizza.id}`} // Navigate to pizza details route
-          style={{ textDecoration: 'none' }} // Optional: Remove underline from links
-        >
-          <Card className="cartita">
-            <Card.Img variant="top" src={pizza.img} />
-            <CardBody>
-              <CardTitle>{pizza.name}</CardTitle>
-              <CardText>{pizza.ingredients.join(", ")}</CardText>
-              <CardText>Pizza ID: {pizza.id}</CardText>
-              <CardText>Price: ${pizza.price}</CardText>
-              <div className="product-buttons">
-                <ButtonsVite 
-                  addToCart={() => addToCart(pizza)} 
-                  removeFromCart={() => removeFromCart(pizza.id)}
-                  pizza={pizza} 
-                />
-              </div>
-            </CardBody>
-          </Card>
-        </Link>
+        <Card key={pizza.id} className="cartita">
+          <Card.Img variant="top" src={pizza.img} />
+          <CardBody>
+            <CardTitle>{pizza.name}</CardTitle>
+            <CardText>{pizza.ingredients.join(", ")}</CardText>
+            <CardText>Pizza ID: {pizza.id}</CardText>
+            <CardText>Price: ${pizza.price}</CardText>
+            <div className="product-buttons">
+              <ButtonsVite 
+                addToCart={() => addToCart(pizza)} 
+                removeFromCart={() => removeFromCart(pizza.id)}
+                pizza={pizza} 
+              />
+              <Link to={`/pizza/${pizza.id}`}>
+                <Button variant="primary">View Details</Button>
+              </Link>
+            </div>
+          </CardBody>
+        </Card>
       ))}
     </div>
   );
 };
 
 export default CardPizza;
+
 
 
