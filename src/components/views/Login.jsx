e2685c0bc32018e19fddf3287142efcf6c8332ae
korzair <ariquelme.snchz@gmail.com>
2024-09-29 14:36:05 -0300

    se limpia codigo

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import LoginForm from '../sub-components/LoginForm';
-// import FormParent from '../sub-components/FormParent';
 export const Login = () => {
   return (
 	
  <>
  <LoginForm />
- {/* <FormParent /> */}
+
  </>
 	
   )
--- src/components/views/Register.jsx
+++ src/components/views/Register.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../css/Register.css';
-// import FormParent from '../sub-components/FormParent';
 import RegisterForm from '../sub-components/RegisterForm';
 
 export const Register = () => {
   return (
     <>
-      {/* <FormParent /> */}
       <RegisterForm />
     </>
   );
